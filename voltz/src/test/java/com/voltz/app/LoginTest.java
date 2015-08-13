730dfc11cf0c33b6332c797cae1a84fc380faff4
Oana Rusu <oana.rusu.qa@gmail.com>
2015-08-13 16:46:38 +0100

    first simple iOS test working

--- voltz/src/test/java/com/voltz/app/LoginTest.java
+++ voltz/src/test/java/com/voltz/app/LoginTest.java
@@ -4,9 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 
 import java.net.MalformedURLException;
@@ -15,30 +18,24 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTest{
 
-	RemoteWebDriver wd;
+    private AppiumDriver<WebElement> driver;
 		
 	@Before
-	public static void main(String[] args) throws MalformedURLException {
+	public void setup() throws MalformedURLException {
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			capabilities.setCapability("appium-version", "1.0");
 			capabilities.setCapability("platformName", "iOS");
 			capabilities.setCapability("platformVersion", "8.4");
 			capabilities.setCapability("deviceName", "iPhone 6");
 			capabilities.setCapability("app", "/Users/oanarusu/Library/Developer/Xcode/DerivedData/Voltz-haijuzfipcoqdjhannyirqpimaam/Build/Products/Debug-iphonesimulator/Voltz.app");
-			RemoteWebDriver wd = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-			wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			wd.close();
+			driver = new IOSDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		}
-	
-	  @After
-	  public void tearDown() throws Exception {
-	    wd.quit();
-	  }
 
 		
 		@Test
 		public void testTest(){
-			wd.findElement(By.name("New user")).click();
+			driver.findElement(By.name("New user")).click();
 
 		}
 
